19ed2c13e10a75f7f63521e93cce4622850e1563
saman <samantacse2k16@gmail.com>
2022-04-29 14:11:54 +0600

    Trial Trial

--- login_page.dart
+++ login_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_app/routes.dart';
 
 class LoginPage extends StatelessWidget {
   const LoginPage({Key? key}) : super(key: key);
@@ -7,47 +8,53 @@ class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Material(
-      child: Column(
-        children: [
-          Image.asset("assets/images/login_img.png", 
-            fit: BoxFit.cover,),
-          SizedBox(
-            height: 20.0,
-          ),
-          Text("Welcome",
-          style: TextStyle(
-            fontWeight: FontWeight.bold,
-            fontSize: 26,
-            //color:  Colors.green,
-          ),
-          ),
-          SizedBox(
-            height: 20.0,
-          ),
-          Padding(
-            padding: const EdgeInsets.symmetric(vertical: 32.0, horizontal: 16.0),
-            child: Column(children: [
-              TextFormField(
-                decoration: InputDecoration(
-                  hintText: "Enter User Name",
-                  labelText: "User Name",
+      child: SingleChildScrollView(
+        child: Column(
+          children: [
+            Image.asset("assets/images/login_img.png", 
+              fit: BoxFit.cover,),
+            SizedBox(
+              height: 20.0,
+            ),
+            Text("Welcome",
+            style: TextStyle(
+              fontWeight: FontWeight.bold,
+              fontSize: 28,
+              //color:  Colors.green,
+            ),
+            ),
+            SizedBox(
+              height: 20.0,
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 32.0, horizontal: 16.0),
+              child: Column(children: [
+                TextFormField(
+                  decoration: InputDecoration(
+                    hintText: "Enter User Name",
+                    labelText: "User Name",
+                  ),
                 ),
-              ),
-              TextFormField(
-                obscureText: true,
-                decoration: InputDecoration(
-                  hintText: "Enter PassWord",
-                  labelText: "Password",
+                TextFormField(
+                  obscureText: true,
+                  decoration: InputDecoration(
+                    hintText: "Enter PassWord",
+                    labelText: "Password",
+                  ),
                 ),
-              ),
-              SizedBox(
-                height: 20.0,
-              ),
-              ElevatedButton(onPressed: (){},
-                  child: Text("Login")),
-            ],),
-          )
-        ],
+                SizedBox(
+                  height: 20.0,
+                ),
+                ElevatedButton(onPressed: (){
+                  Navigator.pushNamed(context, MyRoutes.homeRoute);
+                },
+                    child: Text("Login"),
+                  style: TextButton.styleFrom(minimumSize: Size(150, 40)),
+                ),
+              ],),
+            )
+          ],
+        ),
       )
     );
   }
--- main.dart
+++ main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_app/login_page.dart';
+import 'package:flutter_app/routes.dart';
 
 //import 'package:flutter_app/login_page.dart';
 import 'package:google_fonts/google_fonts.dart';
@@ -33,8 +34,8 @@ class Myapp extends StatelessWidget {
       ),
       routes: {
         "/": (context)=> LoginPage(),
-        "/home": (context) => HomePage(),
-        "/login": (context) => LoginPage(),
+        MyRoutes.homeRoute: (context) => HomePage(),
+        MyRoutes.loginRoute: (context) => LoginPage(),
       },
     );
   }
